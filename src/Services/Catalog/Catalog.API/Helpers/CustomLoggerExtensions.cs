0c9bf6c31b469794b567b4bfa5f23451ca2b1800
Islam Elsherbini <hte.first.fiddler167@gmail.com>
2021-07-18 12:46:37 +0300

    Completed Catalog API with MongoDB

---
+++ src/Services/Catalog/Catalog.API/Helpers/CustomLoggerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Helpers
+{
+    public static class CustomLoggerExtensions 
+    {
+        public static void CustomLogInformation(this ILogger logger)
+        {
+            logger.LogInformation("Constractor Creating");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context) =>
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        public async Task<IEnumerable<Product>> GetProducts() =>
+            await _context.Products.Find(p => true).ToListAsync();
+        public async Task<Product> GetProduct(string id) =>
+            await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        public async Task<IEnumerable<Product>> GetProductByCondation<TField >(Expression<Func<Product, TField>> field, TField value)   =>
+            await _context.Products.Find(Builders<Product>.Filter.Eq(field, value)).ToListAsync();
+        public async Task<IEnumerable<Product>> GetProductByName (string name) => 
+            await _context.Products.Find(Builders<Product>.Filter.Eq(x => x.Name , name)).ToListAsync();
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName) =>
+            await _context.Products.Find(Builders<Product>.Filter.Eq(p => p.Category, categoryName)).ToListAsync();
+        public async Task CreateProduct(Product product) =>
+            await _context.Products.InsertOneAsync(product);
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updateResult = await _context.Products.ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+            return (updateResult.IsAcknowledged && updateResult.ModifiedCount > 0);
+        }
+        public async Task<bool> DeleteProduct(string id)
+        {
+
+            DeleteResult deleteResult = await _context.Products.DeleteOneAsync(Builders<Product>.Filter.Eq(p => p.Id, id));
+            return (deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0);
+        }
+    }
+}
